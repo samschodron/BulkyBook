75306fb2ed7b87f17d7652c3bee57d29949bc06f
schodron@wisc.edu <schodron@wisc.edu>
2023-06-07 16:58:43 -0500

    Add project files.

---
+++ BulkyBookWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
